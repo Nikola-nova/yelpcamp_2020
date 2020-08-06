de392f11a3a9c20509b6bb2e828726ba47343e1d
Nikola-nova <nikola.novakovic1985@gmail.com>
2020-08-06 14:07:40 +0200

    add environment port

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log('The YelpCamp Server Has Started!');
